4499a6be334128e4bec8b945d18d7e12594309c9
roman <51534566+roman-thapa@users.noreply.github.com>
2023-08-25 10:43:08 +0530

    Fetch data from open trivia DB and log it

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,36 @@ import HomePage from './HomePage'
 import Game from './Game'
 
 
-function App() {
+export default function App() {
   const [startQuiz, setStartQuiz] = React.useState(false)
+  const [quizData, setQuizData] = React.useState('')
+  const [newGame, setNewGame] = React.useState(false)
+
+  React.useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5")
+      .then(res => res.json())
+      .then(data => setQuizData(data))
+  }, [startQuiz, newGame])
+  
+  function startNewGame() {
+    setNewGame(prev => !prev)
+  }
+  
+
   function start() {
     setStartQuiz(prev => !prev)
   }
-  console.log(startQuiz)
   return (
     <>
-      {!startQuiz && <HomePage 
-        start={start}
-      />}
-      {startQuiz && <Game />}
-      
+      {!startQuiz ?
+        <HomePage 
+          start={start}
+        /> : 
+        <Game 
+          data={quizData}
+          newGame={startNewGame}
+        />
+      }
     </>
-    
   )
 }
-export default App
--- src/Game.jsx
+++ src/Game.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 
-export default function Game() {
+import TopRight from "./assets/game-tr.svg"
+import BottomLeft from "./assets/game-bl.svg"
+
+export default function Game(props) {
+    const handleClick = () => {
+        props.newGame()
+        console.log(props.data)
+    }
     return (
         <>
-            <h1>
-                Game Started!!!
-            </h1>
+            <img 
+                src={TopRight} 
+                className="top--right"
+            />
+            <div>
+                <h1>
+                    Game Started!!!
+                </h1>
+                <button 
+                    className="start--quiz"
+                    onClick={handleClick}
+                >
+                    New Game
+                </button>
+            </div>
+            <img 
+              src={BottomLeft} 
+              className="bottom--left"
+            />
         </>
     )
 }
