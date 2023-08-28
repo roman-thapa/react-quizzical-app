d89140b6fa8b37178655dfb5d050aa85cf174d29
roman <51534566+roman-thapa@users.noreply.github.com>
2023-08-28 13:28:27 +0530

    Make a new property that will hold choices

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 import he from 'he'
+import { nanoid } from 'nanoid'
 
 import TopRight from "./assets/game-tr.svg"
 import BottomLeft from "./assets/game-bl.svg"
 
 export default function Game(props) {
+
+    (() => {
+        props.data.map(info => {
+            const randomIndex = Math.floor(
+                Math.random() * (info.incorrect_answers.length + 1)
+            )
+            info.choices = info.incorrect_answers.map(choice => choice);
+            info.choices.splice(randomIndex, 0, info.correct_answer)
+        })
+    }) ()
+    
     const handleClick = () => {
         props.newGame()
         console.log(props.data)
     }
 
     function generateQuestions() {
-        return props.data.map((question, index) => (
-            <p key={index}>{he.decode(question.question)}</p>
-          ));
+        return props.data.map((info) => {
+            return (
+                <>
+                    <p key={nanoid}>{he.decode(info.question)}</p>
+                    <hr />
+                </>
+            )
+          });
     }
-
+     
     return (
         <>
             <img 
@@ -26,7 +43,9 @@ export default function Game(props) {
                 <h1>
                     Game Started!!!
                 </h1>
-                <div>{generateQuestions()}</div>
+                <div>
+                    {generateQuestions()}
+                </div>
                 <button 
                     className="start--quiz"
                     onClick={handleClick}
@@ -41,3 +60,45 @@ export default function Game(props) {
         </>
     )
 }
+
+/**[
+    {
+        "category": "Geography",
+        "type": "multiple",
+        "difficulty": "hard",
+        "question": "What is the Finnish word for &quot;Finland&quot;?",
+        "correct_answer": "Suomi",
+        "incorrect_answers": [
+            "Eesti",
+            "Magyarorsz&aacute;g",
+            "Sverige"
+        ],
+        "choices": [
+            "Suomi",
+            "Eesti",
+            "Magyarorsz&aacute;g",
+            "Sverige"
+        ]
+    }
+]*/
+
+/**[
+    {
+        "category": "Geography",
+        "type": "multiple",
+        "difficulty": "hard",
+        "question": "What is the Finnish word for &quot;Finland&quot;?",
+        "correct_answer": "Suomi",
+        "incorrect_answers": [
+            "Eesti",
+            "Magyarorsz&aacute;g",
+            "Sverige"
+        ],
+        "choices": [
+            "Eesti",
+            "Magyarorsz&aacute;g",
+            "Sverige",
+            "Suomi"
+        ]
+    }
+]*/
